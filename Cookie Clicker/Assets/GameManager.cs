905f7a7520962b544203562c0f9ca1f52d335ea3
Martin <mvrt.new@gmail.com>
2025-02-25 17:52:32 +0300

    v0.2
    Added autoclick and passive income mechanics.

--- Cookie Clicker/Assets/GameManager.cs
+++ Cookie Clicker/Assets/GameManager.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Печеньки")]
     [SerializeField] ulong cookies = 0;
+    [Space]
 
+    [Header("Улучшения")]
     [SerializeField] uint granny = 1;
-    [SerializeField] ulong grannyPrice = 100;
-
     [SerializeField] uint bakery = 0;
+    [Space]
+
+    [Header("Цены")]
+    [SerializeField] ulong grannyPrice = 100;
     [SerializeField] ulong bakeryPrice = 5000;
+    [Space]
+
+    [Header("Улучшения - автоклик")]
+    [SerializeField] uint chocolate = 0;
+    [SerializeField] uint orange = 0;
+    [Space]
+
+    [Header("Цены - автоклик")]
+    [SerializeField] ulong chocolatePrice = 1000;
+    [SerializeField] ulong orangePrice = 20000;
+
 
     void Start()
     {
-
+        StartCoroutine(AutoClick());
     }
-    
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -35,11 +51,20 @@ public class GameManager : MonoBehaviour
             BuyBakery();
         }
 
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            BuyChocolate();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            BuyOrange();
+        }
     }
 
     void ProcessClick()
     {
-        cookies += granny + bakery*2;
+        cookies += granny + bakery * 2;
     }
 
     void BuyGranny()
@@ -69,6 +94,46 @@ public class GameManager : MonoBehaviour
         else
         {
             print($"К сожалению, Вам не хватает печенек для покупки. Накопите еще: {bakeryPrice - cookies}");
+        }
+    }
+
+    IEnumerator AutoClick()
+    {
+        while (true)
+        {
+            cookies += chocolate + orange * 3;
+            print(cookies);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    void BuyChocolate()
+    {
+        if (cookies >= chocolatePrice)
+        {
+            cookies -= chocolatePrice;
+            chocolatePrice = (ulong)(chocolatePrice * 1.25f);
+            chocolate++;
+            print($"Поздравляем! Вы прокачали шоколадный вкус. Уровень: {chocolate}");
+        }
+        else
+        {
+            print($"К сожалению, Вам не хватает печенек для покупки. Накопите еще: {chocolatePrice - cookies}");
+        }
+    }
+
+    void BuyOrange()
+    {
+        if (cookies >= orangePrice)
+        {
+            cookies -= orangePrice;
+            orangePrice = (ulong)(orangePrice * 1.25f);
+            orange++;
+            print($"Поздравляем! Вы прокачали апельсиновый вкус. Уровень: {orange}");
+        }
+        else
+        {
+            print($"К сожалению, Вам не хватает печенек для покупки. Накопите еще: {orangePrice - cookies}");
         }
     }
 }
